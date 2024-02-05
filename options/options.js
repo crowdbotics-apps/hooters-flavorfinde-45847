0fc75241171fa4c5e2d6276e4d2bdf48117b76c0
Crowdbotics <team@crowdbotics.com>
2024-02-05 03:59:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hooters-flavorfinde-45847",
+  url: "https://hooters-flavorfinde-45847.botics.co",
+  api: "https://hooters-flavorfinde-45847.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
